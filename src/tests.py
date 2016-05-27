75f08b2f5d2375f5756a15b854762ba2ed2a2637
Hristo Avramov <havramov@asteasolutions.com>
2016-05-27 14:21:28 +0300

    xml and requests modules added

--- src/tests.py
+++ src/tests.py
@@ -1,6 +1,18 @@
 from sklearn import tree
+import requests
+
 X = [[0, 0], [1, 1], [1, 2]]
 Y = [0, 1, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, Y)
 print(clf.predict([2, 2]))
+
+r = requests.get('http://google.com', verify=True)
+print(r.status_code)
+
+from pyquery import PyQuery as pq
+from lxml import etree
+import urllib
+
+d = pq("<html><body>fuaaa</body></html>")
+print(d('body').text())
