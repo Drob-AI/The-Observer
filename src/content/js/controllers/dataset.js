7260e6e39fb5da8604d79b94594cd7af6c5bd4c4
DimiterM <dimiter_m@yahoo.com>
2016-05-28 23:04:59 +0300

    Heatmap added

--- src/content/js/controllers/dataset.js
+++ src/content/js/controllers/dataset.js
@@ -34,12 +34,133 @@ angular.module('observer').
 	      {text: "ut ultrices", weight: 2}
     	];
 
-    	$scope.myJson = {
-		    "type" : 'line',
-		    "background-color" : '#467488',
-		    "series" : [
-		      { "values" : [54,23,34,23,43] },
-		      { "values" : [10,15,16,20,40] }
-		    ]
-		};
+    	fieldnames = ['a1', 'a2', 'a3', 'a4', 'a5']
+    	coefs = [
+    		[1.0, 0.15, 0.35, 0.55, 0.75],
+    		[0.8, 0.15, 0.35, 0.55, 0.75],
+    		[0.6, 0.15, 0.35, 0.55, 0.75],
+    		[0.4, 0.15, 0.35, 0.55, 0.75],
+    		[0.2, 0.15, 0.35, 0.55, 0.75]
+    	]
+
+    	$scope.heatmapJson = {
+            "type":"piano",
+            "title":{
+                "y":"15px",
+                "x":"-10px",
+                "text":"Correlation coefficients",
+                "background-color":"none",
+                "font-family": 'Open Sans Condensed',
+                "font-color":"#05636c",
+                "font-size":"24px",
+                "height":"25px"
+            },
+            "backgroundColor":"#fff",
+            "plotarea":{
+                "margin":"100 60 90 60"
+            },
+            "scaleX":{
+                "zooming":true,
+                "placement":"opposite",
+                "lineWidth":0,
+                "item":{
+                    "border-color":"none",
+                    "size":"13px",
+                	"font-family": 'Open Sans Condensed',
+                    "font-color":"#05636c"
+                },
+                "guide":{
+                    "visible":false
+                },
+                "tick":{
+                    "visible":false
+                },
+                "values":fieldnames
+            },
+            "scaleY":{
+                "zooming":true,
+                "lineWidth":0,
+                "mirrored":true,
+                "tick":{
+                    "visible":false
+                },
+                "guide":{
+                    "visible":false
+                },
+                "item":{
+                    "border-color":"none",
+                    "size":"13px",
+                	"font-family": 'Open Sans Condensed',
+                    "font-color":"#05636c"
+                },
+                "values":fieldnames
+            },
+            /*"legend":{
+                "layout":"x6",
+                "y":"83.5%",
+                "x":"15%",
+                "width":"80%",
+                "shadow":false,
+                "border-width":0,
+                "toggle-action":"none",
+                "item":{
+                    "border-color":"none",
+                    "size":"13px",
+                    "font-color":"#05636c"
+                },
+                "marker":{
+                    "type":"square",
+                    "border-radius":"8",
+                    "border-color":"none",
+                    "size":"13px"
+                },
+                "footer":{
+                    "border-color":"none",
+                    "background-color":"none",
+                    "text-align":"center",
+                    "font-size":"14px",
+                    "font-color":"#05636c"
+                }
+            },*/
+            "zoom":{
+              "background-color":"#ccccff",
+              "border-width":5,
+              "border-color":"#3399ff",
+              "alpha":0.3
+            },
+            "plot":{
+                "aspect":"none",
+                "borderWidth":2,
+                "borderColor":"#eeeeee",
+                "borderRadius":7,
+                "rules":[
+                    {
+                        "rule":"%node-value > 0.8",
+                        "backgroundColor":"#9E2619",
+                        "font-color":"#05636c"
+                    },
+                    {
+                        "rule":"%node-value > 0.6 && %node-value <= 0.8",
+                        "backgroundColor":"#9E6719",
+                        "font-color":"#05636c"
+                    },
+                    {
+                        "rule":"%node-value > 0.4 && %node-value <= 0.6",
+                        "backgroundColor":"#9E8819",
+                        "font-color":"#05636c"
+                    },
+                    {
+                        "rule":"%node-value > 0.2 && %node-value <= 0.4",
+                        "backgroundColor":"#137729",
+                        "font-color":"#05636c"
+                    },
+                    {
+                        "rule":"%node-value > 0 && %node-value <= 0.2",
+                        "backgroundColor":"#183A68",
+                        "font-color":"#05636c"
+                    }
+                ]
+            },
+            "series":coefs.map(function(i) { return {"values":i}; })
+        };
     }]);
