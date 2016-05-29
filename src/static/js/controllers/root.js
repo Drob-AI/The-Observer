e0ae1e4674ab7a17b46f79d88189e6412de45a1d
DimiterM <dimiter_m@yahoo.com>
2016-05-29 11:08:00 +0300

    Login page

--- src/static/js/controllers/root.js
+++ src/static/js/controllers/root.js
@@ -1,4 +1,11 @@
 angular.module('observer').
     controller('RootController', [function () {
-        
+        $('#loginbutton').click(function() {
+			var b =  $(this);
+			b.prop('disabled', true);
+			setTimeout(function() {
+			    b.prop('disabled', false);
+			}, 1000);
+			$('.loginform').toggle();
+		});
     }]);
