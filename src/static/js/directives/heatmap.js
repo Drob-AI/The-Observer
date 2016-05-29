4e9db59b5859a1f9f9aa3d916966b8c2fe094ab6
bladespinner <bladespinner@gmail.com>
2016-05-29 04:53:55 +0300

    Add charts

--- src/static/js/directives/heatmap.js
+++ src/static/js/directives/heatmap.js
@@ -3,135 +3,138 @@ angular.module('observer').
         return {
             restrict: 'A',
             scope: {
-                fields: '=',
-                examples: '='
+                dataset: '='
             },
             replace: true,
             templateUrl: 'views/heatmap.html',
             controller: ['$scope', function ($scope) {
-                fieldnames = datasets[0].fields;
-                coefs = datasets[0].cov_matrix;
+                function render() {
+                    fieldnames = $scope.dataset.fields;
+                    coefs = $scope.dataset.cov_matrix;
 
-                $scope.heatmapJson = {
-                    "type":"piano",
-                    "title":{
-                        "y":"15px",
-                        "x":"-10px",
-                        "text":"Correlation coefficients",
-                        "background-color":"none",
-                        "font-family": 'Open Sans Condensed',
-                        "font-color":"#05636c",
-                        "font-size":"24px",
-                        "height":"25px"
-                    },
-                    "backgroundColor":"#fff",
-                    "plotarea":{
-                        "margin":"100 60 90 60"
-                    },
-                    "scaleX":{
-                        "zooming":true,
-                        "placement":"opposite",
-                        "lineWidth":0,
-                        "item":{
-                            "border-color":"none",
-                            "size":"13px",
-                            "font-family": 'Open Sans Condensed',
-                            "font-color":"#05636c"
-                        },
-                        "guide":{
-                            "visible":false
-                        },
-                        "tick":{
-                            "visible":false
-                        }//,
-                        //"values":fieldnames
-                    },
-                    "scaleY":{
-                        "zooming":true,
-                        "lineWidth":0,
-                        "mirrored":true,
-                        "tick":{
-                            "visible":false
-                        },
-                        "guide":{
-                            "visible":false
-                        },
-                        "item":{
-                            "border-color":"none",
-                            "size":"13px",
-                            "font-family": 'Open Sans Condensed',
-                            "font-color":"#05636c"
-                        }//,
-                        //"values":fieldnames
-                    },
-                    /*"legend":{
-                        "layout":"x6",
-                        "y":"83.5%",
-                        "x":"15%",
-                        "width":"80%",
-                        "shadow":false,
-                        "border-width":0,
-                        "toggle-action":"none",
-                        "item":{
-                            "border-color":"none",
-                            "size":"13px",
-                            "font-color":"#05636c"
-                        },
-                        "marker":{
-                            "type":"square",
-                            "border-radius":"8",
-                            "border-color":"none",
-                            "size":"13px"
-                        },
-                        "footer":{
-                            "border-color":"none",
+                    $scope.heatmapJson = {
+                        "type":"piano",
+                        "title":{
+                            "y":"15px",
+                            "x":"-10px",
+                            "text":"Correlation coefficients",
                             "background-color":"none",
-                            "text-align":"center",
-                            "font-size":"14px",
-                            "font-color":"#05636c"
-                        }
-                    },*/
-                    "zoom":{
-                        "background-color":"#ccccff",
-                        "border-width":5,
-                        "border-color":"#3399ff",
-                        "alpha":0.3
-                    },
-                    "plot":{
-                        "aspect":"none",
-                        "borderWidth":2,
-                        "borderColor":"#eeeeee",
-                        "borderRadius":7,
-                        "rules":[
-                            {
-                                "rule":"%node-value > 0.8",
-                                "backgroundColor":"#9E2619",
+                            "font-family": 'Open Sans Condensed',
+                            "font-color":"#05636c",
+                            "font-size":"24px",
+                            "height":"25px"
+                        },
+                        "backgroundColor":"#fff",
+                        "plotarea":{
+                            "margin":"100 60 90 60"
+                        },
+                        "scaleX":{
+                            "zooming":true,
+                            "placement":"opposite",
+                            "lineWidth":0,
+                            "item":{
+                                "border-color":"none",
+                                "size":"13px",
+                                "font-family": 'Open Sans Condensed',
                                 "font-color":"#05636c"
                             },
-                            {
-                                "rule":"%node-value > 0.5 && %node-value <= 0.8",
-                                "backgroundColor":"#9E6719",
+                            "guide":{
+                                "visible":false
+                            },
+                            "tick":{
+                                "visible":false
+                            }//,
+                            //"values":fieldnames
+                        },
+                        "scaleY":{
+                            "zooming":true,
+                            "lineWidth":0,
+                            "mirrored":true,
+                            "tick":{
+                                "visible":false
+                            },
+                            "guide":{
+                                "visible":false
+                            },
+                            "item":{
+                                "border-color":"none",
+                                "size":"13px",
+                                "font-family": 'Open Sans Condensed',
+                                "font-color":"#05636c"
+                            }//,
+                            //"values":fieldnames
+                        },
+                        /*"legend":{
+                            "layout":"x6",
+                            "y":"83.5%",
+                            "x":"15%",
+                            "width":"80%",
+                            "shadow":false,
+                            "border-width":0,
+                            "toggle-action":"none",
+                            "item":{
+                                "border-color":"none",
+                                "size":"13px",
                                 "font-color":"#05636c"
                             },
-                            {
-                                "rule":"%node-value > 0.2 && %node-value <= 0.5",
-                                "backgroundColor":"#9E8819",
-                                "font-color":"#05636c"
+                            "marker":{
+                                "type":"square",
+                                "border-radius":"8",
+                                "border-color":"none",
+                                "size":"13px"
                             },
-                            {
-                                "rule":"%node-value > -0.5 && %node-value <= 0.2",
-                                "backgroundColor":"#137729",
-                                "font-color":"#05636c"
-                            },
-                            {
-                                "rule":"%node-value > -1.0 && %node-value <= -0.5",
-                                "backgroundColor":"#183A68",
+                            "footer":{
+                                "border-color":"none",
+                                "background-color":"none",
+                                "text-align":"center",
+                                "font-size":"14px",
                                 "font-color":"#05636c"
                             }
-                        ]
-                    },
-                    "series":coefs.map(function(i) { return {"values":i.map(function(j){return j || 0})}; })
-                };
+                        },*/
+                        "zoom":{
+                            "background-color":"#ccccff",
+                            "border-width":5,
+                            "border-color":"#3399ff",
+                            "alpha":0.3
+                        },
+                        "plot":{
+                            "aspect":"none",
+                            "borderWidth":2,
+                            "borderColor":"#eeeeee",
+                            "borderRadius":7,
+                            "rules":[
+                                {
+                                    "rule":"%node-value > 0.8",
+                                    "backgroundColor":"#9E2619",
+                                    "font-color":"#05636c"
+                                },
+                                {
+                                    "rule":"%node-value > 0.5 && %node-value <= 0.8",
+                                    "backgroundColor":"#9E6719",
+                                    "font-color":"#05636c"
+                                },
+                                {
+                                    "rule":"%node-value > 0.2 && %node-value <= 0.5",
+                                    "backgroundColor":"#9E8819",
+                                    "font-color":"#05636c"
+                                },
+                                {
+                                    "rule":"%node-value > -0.5 && %node-value <= 0.2",
+                                    "backgroundColor":"#137729",
+                                    "font-color":"#05636c"
+                                },
+                                {
+                                    "rule":"%node-value > -1.0 && %node-value <= -0.5",
+                                    "backgroundColor":"#183A68",
+                                    "font-color":"#05636c"
+                                }
+                            ]
+                        },
+                        "series":coefs.map(function(i) { return {"values":i.map(function(j){return j || 0})}; })
+                    };
+                }
+                $scope.$watch('dataset', render);
+                render();
             }]
         };
     }]);
--- src/static/js/directives/radar.js
+++ src/static/js/directives/radar.js
@@ -4,72 +4,77 @@ angular.module('observer').
             restrict: 'A',
             scope: {
                 fields: '=',
-                examples: '='
+                statistics: '='
             },
             replace: true,
             templateUrl: 'views/radar.html',
             controller: ['$scope', function ($scope) {
-                radarValues = datasets[0].statstics.map(
-                    function(i){
-                        return (i.type == "string" ?
-                        i.histogram.length :
-                        (i.type == "number" ? (i.std || 0) : 0));
-                    });
-                radarDataBand = datasets[0].fields;
+                function render() {
+                    radarValues = $scope.statistics.map(
+                        function(i){
+                            return (i.type == "string" ?
+                            i.histogram.length :
+                            (i.type == "number" ? (i.std || 0) : 0));
+                        });
+                    radarDataBand = $scope.fields;
 
-                $scope.radarJson = {
-                "globals": {
-                    "font-family":'Open Sans Condensed',
-                    "shadow":false
-                },
-                    "type" : "radar",
-                    "background-color":"#FFF",
-                    "plot":{
-                    "aspect":"rose",
-                    "animation": {
-                        "effect":"ANIMATION_EXPAND_TOP",
-                    "sequence":"ANIMATION_BY_PLOT_AND_NODE",
-                    "speed":30
-                    }
+                    $scope.radarJson = {
+                    "globals": {
+                        "font-family":'Open Sans Condensed',
+                        "shadow":false
                     },
-                    "title" : {
-                    "text" : "Standard Deviation of attributes",
-                    "background-color":"#333",
-                    "font-size":"24px"
-                    },
-                    "scale-k":{
-                    "aspect":"circle",
-                    "visible":false
-                },
-                "scale-v":{
-                    "values":"0:25:5",
-                    "guide": {
-                    "line-width":1,
-                    "line-style":"solid",
-                    "line-color":"#333"
-                    },
-                    "item": {
-                    "color":"#333"
-                    },
-                    "line-color":"#FFF"
-                },
-                "tooltip": {
-                    "text":"%v st.dev. for %data-band",
-                    "background-color":"#CFF",
-                    "color":"#333",
-                    "font-size":"14px"
-                },
-                    "series" : [
-                        {
-                            "values" : radarValues,
-                            "data-band" : radarDataBand,
-                            //"tooltip-text" : "%v studio albums made by %data-band",
-                            "url" : "http://www.google.com/#q=%data-band",
-                            "target" : "_blank",
-                            "background-color":"#0CF"
+                        "type" : "radar",
+                        "background-color":"#FFF",
+                        "plot":{
+                        "aspect":"rose",
+                        "animation": {
+                            "effect":"ANIMATION_EXPAND_TOP",
+                        "sequence":"ANIMATION_BY_PLOT_AND_NODE",
+                        "speed":30
                         }
-                    ]
-                };
+                        },
+                        "title" : {
+                        "text" : "Standard Deviation of attributes",
+                        "background-color":"#333",
+                        "font-size":"24px"
+                        },
+                        "scale-k":{
+                        "aspect":"circle",
+                        "visible":false
+                    },
+                    "scale-v":{
+                        "values":"0:25:5",
+                        "guide": {
+                        "line-width":1,
+                        "line-style":"solid",
+                        "line-color":"#333"
+                        },
+                        "item": {
+                        "color":"#333"
+                        },
+                        "line-color":"#FFF"
+                    },
+                    "tooltip": {
+                        "text":"%v st.dev. for %data-band",
+                        "background-color":"#CFF",
+                        "color":"#333",
+                        "font-size":"14px"
+                    },
+                        "series" : [
+                            {
+                                "values" : radarValues,
+                                "data-band" : radarDataBand,
+                                //"tooltip-text" : "%v studio albums made by %data-band",
+                                "url" : "http://www.google.com/#q=%data-band",
+                                "target" : "_blank",
+                                "background-color":"#0CF"
+                            }
+                        ]
+                    };
+                }
+
+                $scope.$watch('statistics', render);
+                render();
             }]
         };
     }]);
