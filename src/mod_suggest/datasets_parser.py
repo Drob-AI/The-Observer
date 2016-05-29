df9e04a91a2c80ebf57aa4bf374a0efa1c7c608b
Hristo Avramov <havramov@asteasolutions.com>
2016-05-29 10:49:25 +0300

    implements statistic persion test

--- src/mod_suggest/datasets_parser.py
+++ src/mod_suggest/datasets_parser.py
@@ -105,13 +105,13 @@ class DatasetParser:
             dataset_info['cov_matrix'] = None
 
 
-        if(hasattr(dataset_info['static_test'], '__iter__')):
-            for index, field_stats in enumerate(dataset_info['static_test']):
-                    if(np.isnan(field_stats[0])):
-                        dataset_info['static_test'][index] = list(field_stats)
-                        dataset_info['static_test'][index][0] = None
-        else:
-            dataset_info['static_test'] = None
+        # if(hasattr(dataset_info['static_test'], '__iter__')):
+        #     for index, field_stats in enumerate(dataset_info['static_test']):
+        #             if(np.isnan(field_stats[0])):
+        #                 dataset_info['static_test'][index] = list(field_stats)
+        #                 dataset_info['static_test'][index][0] = None
+        # else:
+        #     dataset_info['static_test'] = None
 
         return dataset_info
 
@@ -151,6 +151,7 @@ class DatasetParser:
 
         dataset_info['static_test'] = []
         for numberset in all_number_sets:
+            dataset_info['static_test'].append([])
             for numberset2 in all_number_sets:
-                dataset_info['static_test'].append(stats.pearsonr(numberset, numberset2))
+                dataset_info['static_test'][-1].append(stats.pearsonr(numberset, numberset2))
         return json.dumps(self._clear_info(dataset_info))
